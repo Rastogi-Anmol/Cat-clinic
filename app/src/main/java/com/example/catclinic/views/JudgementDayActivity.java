ad61c28da147f7872a8ed3ff5714d6a3cc487fdc
Rastogi-Anmol <arastog1@ualberta.ca>
2025-06-03 22:43:43 -0600

    completed submission form with correct xml + increased utility

--- app/src/main/java/com/example/catclinic/views/JudgementDayActivity.java
+++ app/src/main/java/com/example/catclinic/views/JudgementDayActivity.java
@@ -2,7 +2,7 @@ package com.example.catclinic.views;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
-import android.content.Intent;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +11,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.example.catclinic.R;
 import com.example.catclinic.controllers.JudgementDayController;
 
@@ -26,7 +21,6 @@ public class JudgementDayActivity extends AppCompatActivity {
 
     private JudgementDayController judgementDayController;
     private EditText thoughtOnTrial, evidenceFor, evidenceAgainst, finalVerdict;
-    private ActivityResultLauncher<Intent> expandedLauncher;
     private Button submitButton;
 
     @Override
@@ -36,21 +30,7 @@ public class JudgementDayActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_judgement_day_activity);
 
-        expandedLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        Intent data = result.getData();
-                        int sourceId    = data.getIntExtra("EXTRA_SOURCE_ID", -1);
-                        String newText  = data.getStringExtra("EXTRA_TEXT");
-                        if (sourceId != -1 && newText != null) {
-                            // Update the correct EditText
-                            EditText target = findViewById(sourceId);
-                            target.setText(newText);
-                        }
-                    }
-                }
-        );
+
 
         judgementDayController = new JudgementDayController(this);
 
@@ -66,6 +46,8 @@ public class JudgementDayActivity extends AppCompatActivity {
 
         submitButton = findViewById(R.id.judgementButton);
 
+
+        //submit button to submit the users input
         submitButton.setOnClickListener((new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
@@ -82,33 +64,8 @@ public class JudgementDayActivity extends AppCompatActivity {
             }
         }));
 
-        thoughtOnTrial.setOnLongClickListener(v -> {
-            openEditor(thoughtOnTrial);
-            return true;
-        });
 
-        evidenceFor.setOnLongClickListener(v -> {
-            openEditor(evidenceFor);
-            return true;
-        });
 
-        evidenceAgainst.setOnLongClickListener(v -> {
-            openEditor(evidenceAgainst);
-            return true;
-        });
 
-        finalVerdict.setOnLongClickListener(v -> {
-            openEditor(finalVerdict);
-            return true;
-        });
 
-    }
-
-    private void openEditor(EditText which) {
-        Intent i = new Intent(this, expanded_textView.class);
-        i.putExtra("EXTRA_SOURCE_ID", which.getId());
-        i.putExtra("EXTRA_TEXT", which.getText().toString());
-        i.putExtra("EXTRA_HINT", which.getHint().toString());
-        expandedLauncher.launch(i);
-    }
-}
+}}
